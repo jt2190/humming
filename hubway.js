ef9bb9ad2199be958da319d21c411ff928bed4f2
James Tikalsky <james@plannedup.com>
2012-08-08 13:44:41 -0400

    Introduced an object for queuing the response data (xml), and letting us know when it's done.

--- hubway.js
+++ hubway.js
@@ -1,12 +1,50 @@
-var http = require('http');
+//mashery, apigee, 
 
-http.get("http://www.thehubway.com/data/stations/bikeStations.xml", function(res) {
-  var chunks = '';
-  res.on('data', function(chunk) {
-    chunks += chunk.toString();
-  }).on('end', function() {
-    console.log(chunks);
+var http = require('http')
+  , util = require('util')
+  , url = "http://www.thehubway.com/data/stations/bikeStations.xml"
+  , events = require('events')
+  , fs = require('fs');
+
+function handleError(e) {
+  console.error(e.message);
+}
+
+function ResponseDataListener(res) {
+
+  if (!(this instanceof ResponseDataListener)) {
+    return new ResponseDataListener(res);
+  } else {
+    events.EventEmitter.call(this); // call the super constructor first  
+    this.data;
+    var self = this;
+    res.on('data', function(chunk) {
+      self.addChunk(chunk);
+    })  
+    res.on('end', function() {
+      self.emit('end', self.toString());
+    })
+    return this;  
+  }
+}
+util.inherits(ResponseDataListener, events.EventEmitter);
+
+ResponseDataListener.prototype.addChunk = function(chunk) {
+  this.data += chunk;
+}
+ResponseDataListener.prototype.toString = function() {
+  return this.data;
+}
+
+http.get(url, function(res) {
+  ResponseDataListener(res).on('end', function(s) {
+    console.log("*** markComplete ***"); 
   });
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
-});
+  res.on('data', function(data) {
+    fs.appendFile('bikeStations.xml', data, function (err) {
+      if (err) throw err;
+      console.log('It\'s saved!');
+    });    
+  })
+  res.on('error', handleError);
+}).on('error', handleError);
